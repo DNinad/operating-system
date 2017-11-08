766ba9045f4bb89cbcde8670e188a71a0ccf3e3a
ninad <nadharmadhikari24@gmail.com>
2017-11-08 17:41:02 +0530

    sem added

--- day6/convarmut.c
+++ day6/convarmut.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include <pthread.h>
 
 int completed=0;
 pthread_mutex_t completemutex;
@@ -14,16 +15,27 @@ void *waitthread(void *data)
 		printf("waiting for update\n");
 	}
 	printf("relase the lock");
+	pthread_mutex_unlock(&completemutex);
 }
 
-void *waitthread(void *data)
+void *upthread(void *data)
 {
 	pthread_mutex_lock(&completemutex);
 	printf("");
 	
 	completed=1;
-	printf("update\n");
-	
-
+	printf("updated the complete\n");
 	printf("relase the lock");
+	pthread_mutex_unlock(&completemutex);
+}
+
+int main()
+{
+	pthread_t waittid,uptid;
+	pthread_mutex_init(&completemutex,NULL);
+	pthread_create(&waittid,NULL,waitthread,NULL);
+	pthread_create(&uptid,NULL,upthread,NULL);
+	pthread_join(waittid,NULL);
+	pthread_join(uptid,NULL);
+	
 }
